6322263fc3275d2faec443bc0940c7b03bfbd65b
ento <ento+github@i.pearlwaffles.xyz>
2023-08-27 21:03:29 -0800

    Extract models

--- src/db_mongo.ts
+++ src/db_mongo.ts
@@ -9,122 +9,22 @@ const mongodb_port = process.env.AOZORA_MONGODB_PORT || '27017';
 const mongodb_replica_set = process.env.AOZORA_MONGODB_REPLICA_SET;
 const mongo_url = `mongodb://${mongodb_credential}${mongodb_host}:${mongodb_port}/aozora`;
 
-import { ATTRS } from './attrs';
+import type { Book as BookBase, Person as PersonBase, StringMap } from './models';
+import { separate_obj } from './models';
 
 //
 // utilities
 //
-type StringMap = { [key: string]: string };
-
-type Book = {
+type Book = BookBase & {
     [index: string]: string | StringMap[];
-
-    book_id?: string;
-    title?: string;
-    title_yomi?: string;
-    title_sort?: string;
-    subtitle?: string;
-    subtitle_yomi?: string;
-    original_title?: string;
-    first_appearance?: string;
-    ndc_code?: string;
-    font_kana_type?: string;
-    copyright?: string;
-    release_date?: string;
-    last_modified?: string;
-    card_url?: string;
-    revisers?: StringMap[];
-    editors?: StringMap[];
-    translators?: StringMap[];
-    authors?: StringMap[];
-    base_book_1?: string;
-    base_book_1_publisher?: string;
-    base_book_1_1st_edition?: string;
-    base_book_1_edition_input?: string;
-    base_book_1_edition_proofing?: string;
-    base_book_1_parent?: string;
-    base_book_1_parent_publisher?: string;
-    base_book_1_parent_1st_edition?: string;
-    base_book_2?: string;
-    base_book_2_publisher?: string;
-    base_book_2_1st_edition?: string;
-    base_book_2_edition_input?: string;
-    base_book_2_edition_proofing?: string;
-    base_book_2_parent?: string;
-    base_book_2_parent_publisher?: string;
-    base_book_2_parent_1st_edition?: string;
-    input?: string;
-    proofing?: string;
-    text_url?: string;
-    text_last_modified?: string;
-    text_encoding?: string;
-    text_charset?: string;
-    text_updated?: string;
-    html_url?: string;
-    html_last_modified?: string;
-    html_encoding?: string;
-    html_charset?: string;
-    html_updated?: string;
 };
 
-type Person = {
-    [index: string]: string;
-    person_id?: string;
-    first_name?: string;
-    last_name?: string;
-    last_name_yomi?: string;
-    first_name_yomi?: string;
-    last_name_sort?: string;
-    first_name_sort?: string;
-    last_name_roman?: string;
-    first_name_roman?: string;
-    date_of_birth?: string;
-    date_of_death?: string;
-    author_copyright?: string;
-};
-
-type BookRolePerson = {
-    book: Book;
-    role: string;
-    person: Person;
+type Person = PersonBase & {
+    [index: string]: string | StringMap[];
 };
 
 type BookList = { [key: string]: Book };
 type PersonList = { [key: string]: Person };
-
-function _separate_obj(entry: StringMap): BookRolePerson {
-    const book: Book = {};
-    const person: Person = {};
-    let role = null;
-
-    ATTRS.forEach((e, i) => {
-        const value = entry[i];
-
-        if (
-            [
-                'person_id',
-                'first_name',
-                'last_name',
-                'last_name_yomi',
-                'first_name_yomi',
-                'last_name_sort',
-                'first_name_sort',
-                'last_name_roman',
-                'first_name_roman',
-                'date_of_birth',
-                'date_of_death',
-                'author_copyright'
-            ].includes(e)
-        ) {
-            person[e] = value;
-        } else if (e === 'role') {
-            role = value;
-        } else {
-            book[e] = value;
-        }
-    });
-    return { book, role, person };
-}
 
 import { IDB } from './i_db';
 
@@ -170,7 +70,7 @@ class DB implements IDB {
         const persons_batch_list: PersonList = {};
         await Promise.all(
             updated.map((entry: StringMap) => {
-                const { book, role, person } = _separate_obj(entry);
+                const { book, role, person } = separate_obj(entry);
                 if (!books_batch_list[book.book_id]) {
                     // book.persons = [];
                     books_batch_list[book.book_id] = book;
---
+++ src/models.ts
@@ -0,0 +1,107 @@
+import { ATTRS } from './attrs';
+
+export type StringMap = { [key: string]: string };
+
+export type Book = {
+    book_id?: string;
+    title?: string;
+    title_yomi?: string;
+    title_sort?: string;
+    subtitle?: string;
+    subtitle_yomi?: string;
+    original_title?: string;
+    first_appearance?: string;
+    ndc_code?: string;
+    font_kana_type?: string;
+    copyright?: string;
+    release_date?: string;
+    last_modified?: string;
+    card_url?: string;
+    revisers?: StringMap[];
+    editors?: StringMap[];
+    translators?: StringMap[];
+    authors?: StringMap[];
+    base_book_1?: string;
+    base_book_1_publisher?: string;
+    base_book_1_1st_edition?: string;
+    base_book_1_edition_input?: string;
+    base_book_1_edition_proofing?: string;
+    base_book_1_parent?: string;
+    base_book_1_parent_publisher?: string;
+    base_book_1_parent_1st_edition?: string;
+    base_book_2?: string;
+    base_book_2_publisher?: string;
+    base_book_2_1st_edition?: string;
+    base_book_2_edition_input?: string;
+    base_book_2_edition_proofing?: string;
+    base_book_2_parent?: string;
+    base_book_2_parent_publisher?: string;
+    base_book_2_parent_1st_edition?: string;
+    input?: string;
+    proofing?: string;
+    text_url?: string;
+    text_last_modified?: string;
+    text_encoding?: string;
+    text_charset?: string;
+    text_updated?: string;
+    html_url?: string;
+    html_last_modified?: string;
+    html_encoding?: string;
+    html_charset?: string;
+    html_updated?: string;
+};
+
+export type Person = {
+    person_id?: string;
+    first_name?: string;
+    last_name?: string;
+    last_name_yomi?: string;
+    first_name_yomi?: string;
+    last_name_sort?: string;
+    first_name_sort?: string;
+    last_name_roman?: string;
+    first_name_roman?: string;
+    date_of_birth?: string;
+    date_of_death?: string;
+    author_copyright?: string;
+};
+
+type BookRolePerson = {
+    book: Book;
+    role: string;
+    person: Person;
+};
+
+export function separate_obj(entry: StringMap): BookRolePerson {
+    const book: Book = {};
+    const person: Person = {};
+    let role = null;
+
+    ATTRS.forEach((e, i) => {
+        const value = entry[i];
+
+        if (
+            [
+                'person_id',
+                'first_name',
+                'last_name',
+                'last_name_yomi',
+                'first_name_yomi',
+                'last_name_sort',
+                'first_name_sort',
+                'last_name_roman',
+                'first_name_roman',
+                'date_of_birth',
+                'date_of_death',
+                'author_copyright'
+            ].includes(e)
+        ) {
+            person[e] = value;
+        } else if (e === 'role') {
+            role = value;
+        } else {
+            book[e] = value;
+        }
+    });
+    return { book, role, person };
+}
